b448e0cf442ccc43bf750109c250be0da94978b6
Sai Charan Kodi <saicharankodi0526@gmail.com>
2026-06-07 12:42:04 +0530

    Add files via upload

---
+++ Arraymanipulation.java
@@ -0,0 +1,47 @@
+package javacalculator;
+
+import java.util.Scanner;
+
+public class Arraymanipulation {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = arr[0];
+        int smallest = arr[0];
+
+        for (int i = 1; i < n; i++) {
+
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+
+        System.out.println("Largest Element: " + largest);
+        System.out.println("Smallest Element: " + smallest);
+
+        System.out.print("Reversed Array: ");
+
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
